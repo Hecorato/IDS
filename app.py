1edf69a88d92ab7c70deceec94a1f6c7591797ae
Hector Garnica <76716586+Hecorato@users.noreply.github.com>
2026-05-15 16:21:05 -0600

    datetime

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.title("Ingreso de Soporte")
 
 df = pd.read_csv('IDS ABRIL-MAYO - Hoja 1.csv')
-
+df['FECHA CREACION'] = pd.to_datetime(df['FECHA CREACION']).dt.date
 df_agrupado = df.groupby('FECHA CREACION')['TICKET'].count().reset_index()
 df_agrupado = df_agrupado.rename(columns={'TICKET': 'TOTAL_TICKETS'})
 
