94f60c0c82bf344a66eff92566100f9e847c94cd
Hector Garnica <76716586+Hecorato@users.noreply.github.com>
2026-05-15 17:58:54 -0600

    titulo

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
+st.title("Dashboard IDS")
 
 # ── CARGA DE DATOS ────────────────────────────────────
 df = pd.read_csv('IDS ABRIL-MAYO - Hoja 1.csv')
