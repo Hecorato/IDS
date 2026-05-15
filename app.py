c71460c0e4867fe700a2a915fa09b4f212c6ef40
Hector Garnica <76716586+Hecorato@users.noreply.github.com>
2026-05-15 14:37:27 -0600

    Grafico_Historico

--- app.py
+++ app.py
@@ -2,5 +2,20 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+st.title("Ingreso de Soporte")
 
-df = pd.read_csv('.csv')
+df = pd.read_csv('IDS ABRIL-MAYO.csv')
+
+df['FECHA CREACION'] = pd.to_datetime(df['FECHA CREACION'])
+
+df_agrupado = df.groupby('FECHA CREACION')['CUENTA'].count().reset_index()
+
+
+fig = px.line(
+    df_agrupado,
+    x='FECHA CREACION',
+    y='CUENTA',
+    title='Cantidad de cuentas por fecha'
+)
+
+st.plotly_chart(fig, use_container_width=True)
