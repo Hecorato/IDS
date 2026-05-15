832ce0b55eae29256bf4d01a755bac8e0b9e625f
Hector Garnica <76716586+Hecorato@users.noreply.github.com>
2026-05-15 17:53:42 -0600

    ajuste de barra

--- app.py
+++ app.py
@@ -77,10 +77,13 @@ with col2:
             hovertemplate='%{y}<br>Fallas: %{x}<extra></extra>',
             textposition='outside'
         )
-        fig1.update_layout(
-        xaxis_title='Fecha', 
-        yaxis_title='Total Tickets',
+        fig2.update_layout(
+        yaxis_title='',
+        xaxis_title='Total Fallas',
+        yaxis=dict(autorange='reversed'),
+        showlegend=False,
         height=500
+)
         )
         
         st.plotly_chart(fig2, use_container_width=True)
