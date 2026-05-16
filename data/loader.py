67e157b5da5f4056834103999b9e4ae6e1f19185
Hector Garnica <76716586+Hecorato@users.noreply.github.com>
2026-05-16 15:21:35 -0600

    se actualiza ruta del dataset

--- data/loader.py
+++ data/loader.py
@@ -1,7 +1,28 @@
 import pandas as pd
 
+CLUSTERS = [
+    "AMPLIACION COACALCO",
+    "AMPLIACION CUAUTITLAN 2",
+    "AMPLIACION MELCHOR OCAMPO 1",
+    "AMPLIACION MELCHOR OCAMPO 2",
+    "AMPLIACION PASEOS DEL VALLE 1",
+    "AMPLIACION SAN PABLO DE LAS SALINAS 2",
+    "COACALCO",
+    "MELCHOR OCAMPO",
+    "PASEOS DEL VALLE",
+    "SAN PABLO DE LAS SALINAS I",
+    "SAN PABLO DE LAS SALINAS II",
+    "TEOLOYUCAN_A",
+    "MELCHOR OCAMPO_A",
+    "TEOLOYUCAN",
+    "TULTEPEC",
+    "VILLA DE LAS FLORES",
+    "TEOLOYUCAN_2_A"
+]
+
 def cargar_datos():
-    df = pd.read_csv('IDS ABRIL-MAYO - Hoja 1.csv')
+    df = pd.read_csv('ids.csv')
+    df = df[df['CLUSTER INSTALACION'].isin(CLUSTERS)]
     df['FECHA CREACION'] = pd.to_datetime(df['FECHA CREACION']).dt.date
     df['FECHA'] = pd.to_datetime(df['FECHA CREACION'])
     return df
