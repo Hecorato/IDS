c197b0cb1d433ed5edaacc7bec16d7dc51f695ba
Hector Garnica <76716586+Hecorato@users.noreply.github.com>
2026-05-18 18:41:41 -0600

    merger optimizado

--- data/merger.py
+++ data/merger.py
@@ -1,15 +1,17 @@
 import pandas as pd
 
 def hacer_merge(df_tickets, df_soluciones):
-    # Seleccionar solo columnas relevantes de soluciones
     cols_soluciones = ['OS', 'Falla', 'Causa', 'Solucion', 'Estatus', 'Cluster']
-    df_sol = df_soluciones[cols_soluciones].copy()
-
-    # Merge por OS
+    # Solo tomar columnas que existan
+    cols_existentes = [c for c in cols_soluciones if c in df_soluciones.columns]
+    df_sol = df_soluciones[cols_existentes].copy()
     df_merge = pd.merge(df_tickets, df_sol, on='OS', how='left')
-
     return df_merge
 
 def cargar_soluciones():
-    df = pd.read_csv('soluciones.csv')
+    df = pd.read_csv(
+        'soluciones.csv',
+        encoding='utf-8-sig',
+        on_bad_lines='skip'
+    )
     return df
