4588ea550728347f619eb3605e40a2c2fe782668
Hector Garnica <76716586+Hecorato@users.noreply.github.com>
2026-05-23 18:43:24 -0600

    ceros

--- pages/validar.py
+++ pages/validar.py
@@ -7,8 +7,8 @@ st.title("Validación Join")
 df_tickets = cargar_datos()
 df_infra = pd.read_csv('semana_detalle_coacalco.csv')
 
-df_tickets['CUENTA'] = df_tickets['CUENTA'].astype(str).str.strip()
-df_infra['Cuenta'] = df_infra['Cuenta'].astype(str).str.strip()
+df_tickets['CUENTA'] = df_tickets['CUENTA'].astype(str).str.strip().str.zfill(10)
+df_infra['Cuenta'] = df_infra['Cuenta'].astype(str).str.strip().str.zfill(10)
 
 df_join = df_tickets.merge(df_infra, left_on='CUENTA', right_on='Cuenta', how='left')
 
