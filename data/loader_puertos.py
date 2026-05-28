b9148290e9ab5955d170cd11369929d2f51a33ae
Hector Garnica <76716586+Hecorato@users.noreply.github.com>
2026-05-27 18:59:02 -0600

    match

--- data/loader_puertos.py
+++ data/loader_puertos.py
@@ -3,13 +3,17 @@ import streamlit as st
 
 @st.cache_data(ttl=3600)
 def cargar_puertos():
-    df = pd.read_csv('puertos.csv', dtype={'CUENTA': str})
-    df['CUENTA'] = df['CUENTA'].str.strip().str.zfill(10)
+    df = pd.read_csv('coacalco_nce.csv')
+    df['CUENTA'] = df['Alias'].str.split('_').str[0].str.strip().str.zfill(10)
+    df['NOMBRE'] = df['Alias'].str.split('_', n=1).str[1]
+    df['FSP'] = df['Frame'].astype(str) + '/' + df['Slot'].astype(str) + '/' + df['Port'].astype(str)
+    df['es_FH'] = df['Terminal Type'].str.contains('FH', na=False)
     df = df.rename(columns={
         'Device Name': 'OLT',
-        'Running Status': 'Estado',
+        'Running Status': 'Estado_ONT',
         'Vendor ID': 'Fabricante',
-        'Terminal Type': 'Modelo'
+        'Terminal Type': 'Modelo',
+        'SN': 'Serie'
     })
-    df['es_FH'] = df['Modelo'].str.contains('FH', na=False)
+    df = df[['CUENTA', 'OLT', 'FSP', 'Estado_ONT', 'Modelo', 'Serie', 'Fabricante', 'es_FH', 'NOMBRE']]
     return df
